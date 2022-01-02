b57762427b34074c9b723c86e15a9c5e79e05628
Abraham (Jon) Moller <mollera2@miamioh.edu>
2022-01-02 01:14:27 -0500

    Add files via upload

--- ti95_send_leonardo.sh
+++ ti95_send_leonardo.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# set serial conditions
+sudo stty -F $2 500000 -icrnl -imaxbel \
+-opost -onlcr \
+-isig -icanon -iexten -echo -echoe -echok -echoctl -echoke
+# now send to the TI-95
 echo "Sending to TI-95..."
 sudo cat $1 > $2
