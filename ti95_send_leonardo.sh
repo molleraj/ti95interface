3931c7c422b95bcf74ba679c9513f53242fff3f3
Abraham (Jon) Moller <mollera2@miamioh.edu>
2023-08-20 19:57:08 -0400

    Update ti95_send_leonardo.sh
    Removed sudo commands before stty and cat that were causing errors in Ubuntu 22.04.

--- ti95_send_leonardo.sh
+++ ti95_send_leonardo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # set serial conditions
-sudo stty -F $2 460800 -icrnl -imaxbel \
+# removed sudo from stty and cat commands below after testing on Ubuntu 22.04
+stty -F $2 460800 -icrnl -imaxbel \
 -opost -onlcr \
 -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke
 # now send to the TI-95
 echo "Sending to TI-95..."
-sudo cat $1 > $2
+cat $1 > $2
